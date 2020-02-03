c443ecacdbb96bfab78989e0d815d1025982eeb7
Petr Messner <petr@leadhub.co>
2020-02-03 11:13:28 +0000

    add bot command /id to retrieve chat id

--- bot.py
+++ bot.py
@@ -13,6 +13,7 @@ from reprlib import repr as smart_repr
 import requests
 import sys
 from textwrap import dedent
+from time import monotonic as monotime
 import yaml
 
 
@@ -46,10 +47,13 @@ class Configuration:
             cfg = yaml.safe_load(cfg_path.read_text())
         else:
             cfg = {}
-        self.bind_host = args.host or cfg.get('bind_host') or '127.0.0.1'
-        self.bind_port = args.port or cfg.get('bind_port') or 5000
-        self.graphql_endpoint = os.environ.get('GRAPHQL_ENDPOINT') or cfg.get('graphql_endpoint')
+        env = os.environ.get
+        self.bind_host = args.host or env('BIND_HOST') or cfg.get('bind_host') or '127.0.0.1'
+        self.bind_port = int(args.port or env('BIND_PORT') or cfg.get('bind_port') or 5000)
+        self.graphql_endpoint = env('GRAPHQL_ENDPOINT') or cfg.get('graphql_endpoint')
+        self.public_url = env('PUBLIC_URL') or cfg.get('public_url')
         self.development_mode_enabled = args.dev
+        self.telegram_api_token = env('TELEGRAM_API_TOKEN') or cfg.get('telegram_api_token')
 
 
 async def async_main(conf):
@@ -58,6 +62,7 @@ async def async_main(conf):
         app = Application()
         app['conf'] = conf
         app['current_alerts'] = current_alerts
+        app['client_session'] = session
         app.router.add_routes(routes)
         runner = AppRunner(app)
         await runner.setup()
@@ -65,6 +70,7 @@ async def async_main(conf):
             site = TCPSite(runner, conf.bind_host, conf.bind_port)
             await site.start()
             logger.info('Listening on http://%s:%s', conf.bind_host, conf.bind_port)
+            await setup_telegram_webhook(conf, session)
             while True:
                 await sleep(10)
                 try:
@@ -73,23 +79,60 @@ async def async_main(conf):
                     logger.info('Failed to retrieve alerts: %s', e)
                     await sleep(60)
                     continue
-                logger.debug('Retrieved %d alerts', len(new_alerts))
                 current_alerts[:] = new_alerts
         finally:
             await runner.cleanup()
 
 
 @routes.get('/')
-@routes.get('/ow-telegram-notifier/')
 async def handle_index(request):
     return Response(text='Hello from ow-telegram-notifier!\n')
 
 
-@routes.get('/ow-telegram-notifier/current-alerts')
+@routes.get('/current-alerts')
 async def handle_list_alerts(request):
     if not request.app['conf'].development_mode_enabled:
         raise HTTPForbidden(text='Available only in development mode')
     return json_response({'current_alerts': request.app['current_alerts']})
+
+
+@routes.post('/telegram-webhook')
+async def handle_telegram_webhook(request):
+    conf = request.app['conf']
+    session = request.app['client_session']
+    data = await request.json()
+    logger.debug('data: %r', data)
+    if data.get('message') and data['message'].get('text') == '/id':
+        chat_id = data['message']['chat']['id']
+        await tg_request(conf, session, 'sendMessage', {
+            'chat_id': chat_id,
+            'text': f'Hola, the chat id is {chat_id}',
+        })
+    return json_response({'ok': True})
+
+
+async def setup_telegram_webhook(conf, session):
+    callback_url = conf.public_url.rstrip('/') + '/telegram-webhook'
+    await tg_request(conf, session, 'setWebhook', {
+        'url': callback_url,
+        'allowed_updates': ['message'],
+    })
+
+
+async def tg_request(conf, session, method_name, params):
+    url = f'https://api.telegram.org/bot{conf.telegram_api_token}/{method_name}'
+    post_kwargs = dict(
+        headers={
+            'Content-Type': 'application/json',
+        },
+        json=params,
+        timeout=30)
+    logger.info('Calling Telegram API method: %s params: %r', method_name, params)
+    async with session.post(url, **post_kwargs) as resp:
+        text = await resp.text()
+        logger.debug('Telegram API response: %s %r', resp.status, text[:1000])
+        resp.raise_for_status()
+        return await resp.json()
 
 
 alert_query = dedent('''
@@ -122,6 +165,7 @@ async def retrieve_alerts(conf, session):
         json={'query': alert_query},
         timeout=30)
     logger.debug('Retrieving alerts from %s', redacted(conf.graphql_endpoint))
+    t0 = monotime()
     async with session.post(conf.graphql_endpoint, **post_kwargs) as resp:
         resp.raise_for_status()
         rj = await resp.json()
@@ -129,6 +173,7 @@ async def retrieve_alerts(conf, session):
         if rj.get('error') or rj.get('errors'):
             raise Exception(f'Received error: {rj}')
         alerts = [edge['node'] for edge in rj['data']['activeAlerts']['edges']]
+        logger.debug('Retrieved %d alerts in %.3f s', len(alerts), monotime() - t0)
         return alerts
 
 
