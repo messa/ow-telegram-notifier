208164208a1cdbd37b68ac61f1dfdee474ef02e0
Petr Messner <petr.messner@gmail.com>
2020-02-10 15:50:32 +0100

    report short-lived alerts in only one message

--- ow_telegram_notifier.py
+++ ow_telegram_notifier.py
@@ -67,7 +67,9 @@ class Configuration:
 async def async_main(conf):
     async with ClientSession() as session:
         current_alerts = await wait_for(retrieve_alerts(conf, session), 30)
+        assert isinstance(current_alerts, list)
         logger.debug('Initial alerts:\n%s', '\n'.join(json.dumps(a) for a in current_alerts))
+        notify_aux = None
         app = Application()
         app['conf'] = conf
         app['current_alerts'] = current_alerts
@@ -88,48 +90,74 @@ async def async_main(conf):
                     logger.info('Failed to retrieve alerts: %s', e)
                     await sleep(60)
                     continue
-                await notify_about_alerts(conf, session, current_alerts, new_alerts)
+                notify_aux = await notify_about_alerts(conf, session, current_alerts, new_alerts, notify_aux)
                 current_alerts[:] = new_alerts
+        except Exception as e:
+            logger.exception('async_main failed: %r', e)
+            await tg_request(conf, session, 'sendMessage', {
+                'chat_id': conf.telegram_chat_id,
+                'text': f'Exception: `{tg_md2_escape(repr(e))}`',
+                'parse_mode': 'MarkdownV2',
+            })
         finally:
             await runner.cleanup()
 
 
-async def notify_about_alerts(conf, session, old_alerts, new_alerts):
+async def notify_about_alerts(conf, session, old_alerts, new_alerts, notify_aux=None):
     if not conf.telegram_chat_id:
         logger.info('telegram_chat_id not configured')
         return
-    message_texts = generate_message_texts(old_alerts, new_alerts)
+    message_texts, notify_aux = generate_message_texts(old_alerts, new_alerts, notify_aux)
     for txt in message_texts:
         await tg_request(conf, session, 'sendMessage', {
             'chat_id': conf.telegram_chat_id,
             'text': txt,
             'parse_mode': 'MarkdownV2',
         })
+    return notify_aux
 
 
-def generate_message_texts(old_alerts, new_alerts):
-    old_alerts_by_id = {a['alertId']: a for a in old_alerts}
-    new_alerts_by_id = {a['alertId']: a for a in new_alerts}
-    assert len(old_alerts_by_id) == len(old_alerts)
-    assert len(new_alerts_by_id) == len(new_alerts)
-    message_texts = []
+def generate_message_texts(previous_alerts, current_alerts, notify_aux):
+    '''
+    Parameter notify_aux are data thta this function uses to keep track of things.
+    '''
+    if not notify_aux:
+        notify_aux = {
+            'waiting_alert_ids': {}, # alert id -> monotime
+        }
+    old_alerts_by_id = {a['alertId']: a for a in previous_alerts}
+    new_alerts_by_id = {a['alertId']: a for a in current_alerts}
+    assert len(old_alerts_by_id) == len(previous_alerts)
+    assert len(new_alerts_by_id) == len(current_alerts)
+    closed_alerts = [a for a in old_alerts if a['alertId'] not in new_alerts_by_id]
+    opened_alerts = [a for a in new_alerts if a['alertId'] not in old_alerts_by_id]
+    mentions_of_closed_alerts = []
+    mentions_of_short_lived_alerts = []
+    mentions_of_opened_alerts = []
     # message with closed alerts
-    text_parts = []
-    for a in old_alerts:
-        if a['alertId'] not in new_alerts_by_id:
-            logger.debug('Alert closed: %s', json.dumps(a))
-            text_parts.append('\U0001F334 ' + alert_text(a))
-    if text_parts:
-        message_texts.append('\n'.join(text_parts))
+    for a in closed_alerts:
+        if a['alerId'] in notify_aux['waiting_alert_ids']:
+            notify_aux['waiting_alert_ids'].pop(a['alertId'])
+            mentions_of_short_lived_alerts.append('\u267B ' + alert_text(a))
+        else:
+            mentions_of_closed_alerts.append('\U0001F334 ' + alert_text(a))
     # message with newly opened alerts
-    text_parts = []
-    for a in new_alerts:
-        if a['alertId'] not in old_alerts_by_id:
-            logger.debug('Alert open: %s', json.dumps(a))
-            text_parts.append('\U0001F525 ' + alert_text(a))
-    if text_parts:
-        message_texts.append('\n'.join(text_parts))
-    return message_texts
+    for a in opened_alerts:
+        assert a['alertId'] not in notify_aux['waiting_alerts']
+        notify_aux['waiting_alerts'][a['alertId']] = monotime() + 60
+    for a in current_alerts:
+        if notify_aux['waiting_alerts'].get(a['alertId']):
+            if notify_aux['waiting_alerts'][a['alertId']] <= monotime():
+                notify_aux['waiting_alert_ids'].pop(a['alertId'])
+                mentions_of_opened_alerts.append('\U0001F525 ' + alert_text(a))
+    assert notify_aux['waiting_alerts'].keys() <= {a['alertId'] for a in current_alerts}
+    message_texts = [
+        '\n'.join(mentions_of_closed_alerts),
+        '\n'.join(mentions_of_short_lived_alerts),
+        '\n'.join(mentions_of_opened_alerts),
+    ]
+    message_texts = [t for t in message_texts if t]
+    return message_texts, notify_aux
 
 
 def tg_md2_escape(s):
